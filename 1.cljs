1c999cf79b175b36037416f583266c2adaa8b024
Andrew J. Pierce <apierce@openwhere.com>
2016-11-30 08:32:51 -0500

    Add problem to the top of solution 1

--- 1.cljs
+++ 1.cljs
@@ -1,4 +1,10 @@
 #!/usr/bin/env planck
+;;;;;;;;;;
+;  If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+;  get 3, 5, 6 and 9. The sum of these multiples is 23.
+;
+;  Find the sum of all the multiples of 3 or 5 below 1000.
+;;;;;;;;;;
 
 (defn mod3or5? [number]
   (or
