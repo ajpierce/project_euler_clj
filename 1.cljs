1fe6f0c7361d951d2d677dcdcdffe7a28af0281c
Andrew J. Pierce <apierce@openwhere.com>
2016-11-30 08:33:29 -0500

    Clean solution 1 filter usage; comment out test answer

--- 1.cljs
+++ 1.cljs
@@ -11,10 +11,10 @@
     (= 0 (mod number 3))
     (= 0 (mod number 5))))
 
-(defn solution [rangeLimit]
-  ( ->> (range rangeLimit)
-      (filter #(mod3or5? %))
+(defn solution [limit]
+  ( ->> (range limit)
+      (filter mod3or5?)
       (reduce +)))
 
-(println "Sum of all multiples of 3 and 5 below 10? " (solution 10))
+; (println "Sum of all multiples of 3 and 5 below 10? " (solution 10))
 (println "Sum of all multiples of 3 and 5 below 1000? " (solution 1000))
