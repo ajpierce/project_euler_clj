46a208df229d215496a60e0a848d5caced75d958
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2016-12-04 21:34:57 -0500

    Add file containing common functions to use across problems

---
+++ euler/common.cljs
@@ -0,0 +1,28 @@
+(ns euler.common)
+
+(defn pow
+  "Raise `base` to the power of `exp`
+    eg: (pow 2 3) -> 8"
+  [base exp]
+  (apply * (repeat exp base)) )
+
+(defn factor?
+  "Test to see if candidate is a factor of number. Returns a bool.
+    eg: (factor? 15 3) -> true
+    eg: (factor? 16 3) -> false"
+  [number candidate]
+  (zero? (mod number candidate)) )
+
+(defn prime-factors [number]
+  "For a given number, return a vector of the prime factors.
+    eg: 18 -> [2 3 3]
+    eg: 7 -> [7]
+    eg: -4 -> []"
+  (loop [quotient number
+         divisor 2
+         factors []]
+    (if (< quotient 2)
+      factors
+      (if (= 0 (rem quotient divisor))
+        (recur (/ quotient divisor) divisor (conj factors divisor))
+        (recur quotient (inc divisor) factors) ))))
