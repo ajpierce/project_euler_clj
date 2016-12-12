42254b3d0775d0f5ace493e29cee4992ae1f4544
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2016-12-12 08:55:56 -0500

    Convert to leinengen project to get access to clojure libs
    This means no more easy clojurescript for everything anymore.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject euler "0.1.0-SNAPSHOT"
+  :description "Solutions to Project Euler problems solved in Clojure(Script)"
+  :url "https://github.com/ajpierce/project_euler_clj"
+  :license {:name "MIT"
+            :url "https://github.com/ajpierce/project_euler_clj/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.combinatorics "0.1.3"]
+                 ])
