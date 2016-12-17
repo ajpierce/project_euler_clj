fa3151f6c01e378db56c67ae9a81064c72b00b31
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2016-12-17 15:51:32 -0500

    Convert solution 007 from CLJS to CLJ

---
+++ src/euler/solutions/007.clj
@@ -0,0 +1,6 @@
+(ns euler.solutions.007
+  "By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+  What is the 10001st prime number?"
+  (:require [euler.math.primes :refer [primes]]) )
+
+(time (println "Solution 007:" (nth (sort (primes 120000)) 10000)))
