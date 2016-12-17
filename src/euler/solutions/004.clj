aa66025d0d24ac67fbfc83843612bc28e329ae5a
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2016-12-17 15:13:06 -0500

    Convert solution 004 from CLJS to CLJ
    Also speed up execution by nearly 4x by using (set) instead of (unique)

--- src/euler/solutions/004.clj
+++ src/euler/solutions/004.clj
@@ -1,12 +1,8 @@
-#!/usr/bin/env planck
-;;;;;;;;;;
-;  A palindromic number reads the same both ways.
-;  The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-;
-;  Find the largest palindrome made from the product of two 3-digit numbers.
-;;;;;;;;;;
+(ns euler.solutions.004
+  "A palindromic number reads the same both ways.
+  The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
-(ns solutions.four
+  Find the largest palindrome made from the product of two 3-digit numbers."
   (:require [clojure.string :refer [join]]) )
 
 ; Reverse either numbers or strings, but NOT sequences or iterables!
@@ -18,13 +14,13 @@
 (def three-digit-numbers (range 100 1000))
 
 (def three-digit-multiples
-  (distinct
+  (set
     (for [x three-digit-numbers
           y three-digit-numbers]
       (* x y) )))
 
-(time (println
-  (->> three-digit-multiples
-       (filter palindrome?)
-       (apply max)
-  )))
+(->> three-digit-multiples
+     (filter palindrome?)
+     (apply max)
+     (println "Solution 004:")
+     (time))
