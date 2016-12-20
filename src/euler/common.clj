517c11da296bbbd0c817ef5a817adddebacc78f3
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2016-12-19 20:59:03 -0500

    Move "proper-divisors" into the common lib

--- src/euler/common.clj
+++ src/euler/common.clj
@@ -1,4 +1,8 @@
-(ns euler.common "A collection of helper functions")
+(ns euler.common "A collection of helper functions"
+  (:require [euler.math.primes :refer [factors]]) )
 
 (defn parse-int [s]
   (Integer/parseInt (re-find #"\A-?\d+" s)))
+
+(defn proper-divisors [n]
+  (disj (factors n) n) )
--- src/euler/solutions/021.clj
+++ src/euler/solutions/021.clj
@@ -5,10 +5,7 @@
   For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
   Evaluate the sum of all the amicable numbers under 10000."
-  (:require [euler.math.primes :refer [factors]]) )
-
-(defn proper-divisors [n]
-  (disj (factors n) n) )
+  (:require [euler.common :refer [proper-divisors]]) )
 
 (defn amicable? [a]
   (let [b (reduce + (proper-divisors a))]
