fc60a5860f176f371c98fb6d4396e00ed5a7d47a
Andrew J. Pierce <andrew.j.pierce@gmail.com>
2016-12-19 21:56:27 -0500

    Add solution to problem 025

---
+++ src/euler/solutions/025.clj
@@ -0,0 +1,11 @@
+(ns euler.solutions.024 "What is the index of the first term in the Fibonacci sequence to contain 1000 digits?")
+
+(def fib (lazy-cat [(bigint 1) 1] (map + fib (rest fib))))
+
+(defn num-digits [n] (count (str n)))
+
+(->> (take-while (fn [n] (< (num-digits n) 1000)) fib)
+     (count)
+     (inc)
+     (println "Solution 025:")
+     (time))
