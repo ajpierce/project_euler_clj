4670083b49c0179c055352e7c6e25397ee6d87fe
Andrew J. Pierce <apierce@openwhere.com>
2017-01-13 23:33:29 -0500

    Add (coll-to-int) fn to common.clj

--- src/euler/common.clj
+++ src/euler/common.clj
@@ -1,8 +1,13 @@
 (ns euler.common "A collection of helper functions"
+  (:require [clojure.string :refer [join]])
   (:require [euler.math.primes :refer [factors]]) )
 
 (defn parse-int [s]
   (Integer/parseInt (re-find #"\A-?\d+" s)))
+
+(defn coll-to-int "Converts a collection of numbers to an int. Cannot handle nested collections (because adding a flatten made it much slower)"
+  [coll]
+  (->> coll (join "") parse-int) )
 
 (defn proper-divisors [n]
   (disj (factors n) n) )
